2e1a107331c8435d8748fdb9db346dda4b951ae1
Hieu <hieutrungpham91@gmail.com>
2025-01-30 17:46:03 +0200

    fetching data

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,25 +1,46 @@
-import { db } from "@vercel/postgres";
+// import { db } from "@vercel/postgres";
 
-const client = await db.connect();
+// const client = await db.connect();
 
-async function listInvoices() {
-  const data = await client.sql`
-    SELECT invoices.amount, customers.name
-    FROM invoices
-    JOIN customers ON invoices.customer_id = customers.id
-    WHERE invoices.amount = 666;
-  `;
+// async function listInvoices() {
+//   const data = await client.sql`
+//     SELECT invoices.amount, customers.name
+//     FROM invoices
+//     JOIN customers ON invoices.customer_id = customers.id
+//     WHERE invoices.amount = 666;
+//   `;
 
-  return data.rows;
-}
+//   return data.rows;
+// }
+
+// export async function GET() {
+//   // return Response.json({
+//   //   message:
+//   //     'Uncomment this file and remove this line. You can delete this file when you are finished.',
+//   // });
+//   try {
+//     return Response.json(await listInvoices());
+//   } catch (error) {
+//     return Response.json({ error }, { status: 500 });
+//   }
+// }
+
+import { createClient } from "@supabase/supabase-js";
+import postgres from "postgres";
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
+
+const data = await sql`SELECT * FROM customers where name = `;
+// const supabase = createClient(
+//   process.env.SUPABASE_URL!,
+//   process.env.SUPABASE_SERVICE_ROLE_KEY!
+// );
+
+// const { data, error } = await supabase.from("revenue").select();
 
 export async function GET() {
-  // return Response.json({
-  //   message:
-  //     'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  // });
   try {
-    return Response.json(await listInvoices());
+    return Response.json(data);
   } catch (error) {
     return Response.json({ error }, { status: 500 });
   }
